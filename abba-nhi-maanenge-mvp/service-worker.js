403da74155e9fdb56e9f22241c0a1e3e45519b8e
DeepakLinux <deepakvishwakarmajs@gmail.com>
2026-06-11 18:57:34 +0530

    remove tabs add

---
+++ abba-nhi-maanenge-mvp/service-worker.js
@@ -0,0 +1,82 @@
+// delete the tab single
+
+// create a tab
+// chrome.runtime.onInstalled.addListener(
+//     (object) => {
+//        if(object["reason"]==="install")
+//        {
+//         chrome.tabs.create(
+//             {
+//                 url:"https://youtube.com/@deepak420re4"
+//             }
+//         )
+//        }
+//     }
+// )
+
+async function getCurrentTab(params) {
+    let queryOptions={active:false,lastFocusedWindow:true}
+    let tab=await chrome.tabs.query(queryOptions)
+    console.log(tab);
+    
+    return tab
+}
+
+getCurrentTab()
+// async function  always return promise we cautious while using it
+
+chrome.commands.onCommand.addListener((command) => {
+  if(command==="panic-key")
+  {
+    GetallTab()
+  }
+});
+
+
+
+function GetallTab()
+{
+let alltabsPromise=getCurrentTab()
+alltabsPromise.then(
+    (restultofAlltab) => 
+        {   
+            console.log("the all tab are now at this time",restultofAlltab);
+            
+            let AlltabId=restultofAlltab.map(
+                (tabInfo) => tabInfo.id 
+            )
+            removeAllActiveTab(AlltabId)
+        }
+            
+)
+}
+
+function removeAllActiveTab(arrayOfallTabIds)
+{
+chrome.tabs.remove(
+        arrayOfallTabIds
+).then(
+    (response) => {
+        console.log('tabs are cleared')
+        chrome.tabs.create(
+            {
+                url:"https://instagram.com/whileddia"
+            }
+        ).then(
+            (data) => console.log('tab is created')
+        ).catch(
+            () => console.error('error occured')
+        )
+    }
+).catch(
+    (err) => console.error('something went wrong')
+)
+}
+// chrome.runtime.onInstalled.addListener(({reason}) => {
+//   if (reason === 'install') {
+//     chrome.tabs.create({
+//       url: "onboarding.html"
+//     });
+//   }
+// });
+
