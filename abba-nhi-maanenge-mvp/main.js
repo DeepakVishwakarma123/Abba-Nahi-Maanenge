cc5f6e1f1373853c348a468dd10b3ddaccf297c5
DeepakLinux <deepakvishwakarmajs@gmail.com>
2026-06-21 20:45:12 +0530

    base strcture of profie data add

--- abba-nhi-maanenge-mvp/main.js
+++ abba-nhi-maanenge-mvp/main.js
@@ -2,16 +2,50 @@ import { saveProfileToLocal,AllProfiles,getProfileData } from "./Hooks/local-sto
 
 
 let buttonSetting=document.querySelector("#buttonSetting")
-  let closeButton=document.querySelector("#closeButton")
+let closeButton=document.querySelector("#closeButton")
+let profileName=document.querySelector("#profileName")
+let url=document.querySelector("#url")
+let addButton=document.querySelector(".addButton")
 
 
-        let sideMenu=document.querySelector(".sideMenu")
-        function moveToleft()
-        {
-            sideMenu.classList.add('moveToleft')
-        }
-        function hideTheMenu(){
-            sideMenu.classList.remove('moveToleft')
-        }
-        buttonSetting.addEventListener('click',moveToleft)
-        closeButton.addEventListener('click',hideTheMenu)
+function saveUrlToCurrentProfile()
+{    
+    let profileNameString=profileName.value
+    let userCustomUrl=url.value
+
+    console.log(profileNameString);
+    
+
+
+    let urlObject={
+        [url]:userCustomUrl
+    }
+    let arrayOfUrlObjects=[]
+    arrayOfUrlObjects.push(urlObject)
+    let currentProfile={
+        [profileNameString]:arrayOfUrlObjects,
+        isActive:true
+    }
+    console.log("current Profile is now",currentProfile);
+    
+    //first of all get the data
+    let savedAllProfiles=getProfileData("AllProfiles")
+    savedAllProfiles.then(
+        (savedProfiles) => console.log("all saved Profiles are now",savedProfiles)
+        ).catch(
+            (err) => console.error("error",err)
+        )
+}
+
+
+let sideMenu=document.querySelector(".sideMenu")
+function moveToleft()
+{
+    sideMenu.classList.add('moveToleft')
+}
+function hideTheMenu(){
+    sideMenu.classList.remove('moveToleft')
+}
+buttonSetting.addEventListener('click',moveToleft)
+closeButton.addEventListener('click',hideTheMenu)
+addButton.addEventListener('click',saveUrlToCurrentProfile)
